55ccea63fc5098db421e19564b85b7bae2d706e5
Sayed <sayedsakkaf1@hotmail.com>
2022-05-18 13:09:52 +0100

    Added test data to movie-controller

--- api/v1/controller/movie-controller.js
+++ api/v1/controller/movie-controller.js
@@ -1,6 +1,8 @@
 const MovieNotFoundError = require('../errors/movie-not-found-error.js');
 const Movie = require('../models/movies.js');
 
+const Movies = [new Movie({ title: "Fast and Furious", description:"Cars driving fast and dangerous", genre:"Action", releaseDate:"2001", runtime:"106"})];
+
 
 module.exports = {
 
