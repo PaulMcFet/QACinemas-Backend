644ab8fcdd9e73c4e07fcb5817bb4a5308e0cf9e
Sayed <sayedsakkaf1@hotmail.com>
2022-05-23 10:41:28 +0100

    Added bcrypt functionality to hash passwords

--- api/v1/models/users.js
+++ api/v1/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema ({
 firstName: {
@@ -20,14 +21,30 @@ email: {
     trim: true
 },
 
+password: {
+    type: String, 
+    required: true,
+    trim: true
+
+},
+
 admin: {
     type: Boolean,
     default: false
 }
 
-
 });
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre ('save', async function (next){
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error)
+    }
+})
 
+const User = mongoose.model('User', userSchema)
 module.exports = User;
